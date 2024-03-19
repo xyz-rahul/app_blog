dd8476ac0859861ea62717f5c0f6d9d9907519ee
Rahul Kumar <me.rahulkumar@icloud.com>
2024-03-19 11:31:30 +0530

    updated view page

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -13,42 +13,6 @@ export default async function page({
                 <h1 className="text-5xl font-bold leading-none">
                     {blog.title}
                 </h1>
-                <div className="flex flex-wrap space-x-2 text-sm dark:text-gray-400">
-                    <a
-                        rel="noopener noreferrer"
-                        href="#"
-                        className="p-1 hover:underline"
-                    >
-                        #MambaUI
-                    </a>
-                    <a
-                        rel="noopener noreferrer"
-                        href="#"
-                        className="p-1 hover:underline"
-                    >
-                        #TailwindCSS
-                    </a>
-                    <a
-                        rel="noopener noreferrer"
-                        href="#"
-                        className="p-1 hover:underline"
-                    >
-                        #Angular
-                    </a>
-                </div>
-                <p className="text-sm dark:text-gray-400">
-                    by
-                    <a
-                        href="#"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:underline dark:text-violet-400"
-                    >
-                        <span>Leroy Jenkins</span>
-                    </a>
-                    on
-                    <span>Feb 12th 2021</span>
-                </p>
             </div>
 
             <div className="dark:text-gray-100 text-sm">
